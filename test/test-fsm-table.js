640f7e24817f9031a480ce4a6c40d1b2a020c682
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-04 05:32:32 +0200

    q

--- test/test-fsm-table.js
+++ test/test-fsm-table.js
@@ -1,21 +1,22 @@
 var assert = require('assert');
 var fsm = require('../lib/fsm-table');
 
-
 fsm.addState('Init');
 fsm.addState('Calculate');
 fsm.addState('Final');
+
 fsm.setData('do_calc', {calc:true});
 
 fsm.addTransition('Init', 'Calculate', function() {
-	data = fsm.getData('do_calc');
-	if (data.calc == true) {
-		return 'Calculate';
-	}
-	else return 'Final';
+	fsm.setData('secret', 'foo');
+	return 'Calculate';
 });
-fsm.addTransition('Calculate', 'Finish', function() {
+
+fsm.addTransition('Calculate', 'Final', function() {
+	var data = fsm.getData('secret');
+	assert.ok(data == 'foo');
 	return 'Final';
 });
+
 fsm.run();
 
