926300354ecd09584c5e89086ce660777c006a26
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-11 22:13:19 +0200

    - adding docs

--- test/test-fsm-table.js
+++ test/test-fsm-table.js
@@ -1,22 +1,22 @@
 var assert = require('assert');
 var fsm = require('../lib/fsm-table');
-
+// first of all we ad states
 fsm.addState('Init');
 fsm.addState('Calculate');
 fsm.addState('Final');
-
+// we set some data required for the fsm
 fsm.setData('do_calc', {calc:true});
-
+// we add a transition code, this one sets some data
 fsm.addTransition('Init', 'Calculate', function() {
 	fsm.setData('secret', 'foo');
 	return 'Calculate';
 });
-
+// add another transition
 fsm.addTransition('Calculate', 'Final', function() {
 	var data = fsm.getData('secret');
 	assert.ok(data == 'foo');
 	return 'Final';
 });
-
+// run the machine 
 fsm.run();
 
