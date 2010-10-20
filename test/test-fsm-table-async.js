6f63fc76c90d9f4799ed05a1546ad098c418fbf1
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-20 22:46:18 +0200

    - basics for the async table

--- test/test-fsm-table-async.js
+++ test/test-fsm-table-async.js
@@ -1,2 +1,17 @@
 var assert = require('assert');
-var table = require('../lib/fsm-table-async');
+var sys = require('sys');
+var table = require('../lib/fsm-table-async').asyncTable;
+
+var myFsm = new table(function() {
+    sys.log('init');
+});
+
+assert.ok(typeof myFsm == 'object');
+
+var me = myFsm;
+myFsm.run(function() {
+    sys.log('run');
+    me.emitter.emit('end');    
+});
+
+
