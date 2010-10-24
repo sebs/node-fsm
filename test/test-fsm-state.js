889cd024af7671821a4d4dea3ea82da48fc32f09
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-24 04:31:01 +0200

    - removed the action attribut to reflect which action is executed, we do not need this, the callback we are in reflects which action is in progress with execution

--- test/test-fsm-state.js
+++ test/test-fsm-state.js
@@ -11,27 +11,18 @@ assert.ok(typeof cState.events.transition == 'object');
 assert.ok(typeof cState.events.input == 'object');
 // on - the event adding  function
 assert.ok(typeof cState.on == 'function');
-// action executed atm 
-assert.ok(cState.action == 'input');
 
 // add an event that has a method passed
 assert.ok(cState.on('entry', function() {
-    // sys.log(cState.action);
-    // assert.ok(cState.action == 'entry');
     me.emit('exit');    
 }));
 assert.ok(cState.on('exit', function() {
-    // sys.log(cState.action);
-    // assert.ok(me.action == 'exit');
     me.emit('transition');    
 }));
 assert.ok(cState.on('transition', function() {
-    // sys.log(cState.action);
-    // assert.ok(me.action == 'transition');
+
 }));  
 assert.ok(cState.on('input', function() {
-    sys.log(cState.action);
-    // assert.ok(me.action == 'input');
     me.emit('entry');
 }));
 
