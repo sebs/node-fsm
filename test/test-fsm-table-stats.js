8bd45b50e5a427575bf9339c791ac15f3d8a74f1
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-25 01:19:13 +0200

    tests for status function

---
+++ test/test-fsm-table-stats.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var fsm = require('../lib/fsm-table-async').asyncTable;
+var state = require('../lib/fsm-state');
+// first of all we ad states
+var myFsm = new fsm(function() {});
+var stats = myFsm.stats();
+assert.ok(typeof stats == 'object');
+
+
