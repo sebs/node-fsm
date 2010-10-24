bb06b1e03fc52b957babf2faaca910880cc650a2
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-25 01:27:34 +0200

    - moved Stuff

---
+++ test/lib/fsm/test-async-count.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var state = require('../lib/fsm-state').state;
+var fsm = require('../lib/fsm-table-async').asyncTable;
+var sys = require('sys');
+// now add all the states to the fsm
+var myFsm = new fsm(function() {});
+assert.ok(myFsm.counter == 0);
+
+var foundE = new state(); 
+foundE.on('input', function() {
+    foundE.emit('transition');
+    assert.ok(myFsm.counter == 2); 
+});
+
+foundE.on('transition', function() {
+    myFsm.emitState('win');
+});
+
+var win = new state();
+win.on('input', function() {
+    // sys.log('i win at finding agile win');
+});
+
+myFsm.on('win', win);
+myFsm.on('found_e', foundE);
+myFsm.execute('found_e');
+assert.ok(myFsm.counter == 2);
+
---
+++ test/lib/state/test-fsm-state-storage.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var sys = require('sys');
+var state = require('../lib/fsm-state').state;
+var myState = new state();
+assert.ok(myState.setPayLoad({id:1}));
+var data = myState.getPayLoad();
+assert.ok(typeof data == 'object');
+assert.ok(data.id == 1);
