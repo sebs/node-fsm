58b9acd89676eb135a2e9d91d1e6647ab5c72f88
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-25 06:44:15 +0200

    - working on the async end

--- test/test-table-stats.js
+++ test/test-table-stats.js
@@ -1,5 +1,6 @@
 var fsm = require('./common').fsm;
 var state = require('./common').state;
+var sys = require('sys');
 /**
  * Module dependencies.
  */
@@ -13,6 +14,10 @@ module.exports = {
         assert.type(stats.ticks, 'number');
         assert.type(stats.time.tick, 'object');
         assert.type(stats.time.created, 'object');
+        // sys.log(JSON.stringify(stats)); 
+        var testState = new state();
+        myFsm.on('teststate', testState);
+        myFsm.execute('teststate');
     }, 
 };
 
