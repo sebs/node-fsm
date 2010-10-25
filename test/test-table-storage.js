34cffbac7c5ae78c1b525dc34ab39720532e7d20
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-25 03:54:18 +0200

    - cloud9 hates me

--- test/test-table-storage.js
+++ test/test-table-storage.js
@@ -4,7 +4,7 @@ var state = require('./common').state;
 module.exports = {
     'bar()': function(assert){
         myFsm = new fsm(function(){});
-        myFsm.setData({id:1});
+        myFsm.setData({id:1}); 
         var data = myFsm.getData();
         assert.ok(data.id == 1);
     }
